40e52a4212f261fe7e4dcdcd705a2dc1c885b670
Kraze <joaotbarros9@gmail.com>
2023-12-17 21:07:49 +0000

    commit: connection fixed

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const app = express()
 require('dotenv').config()
 app.use(express.json())
 const corsOptions = {
-  origin: 'http://127.0.0.1:5173', 
+  origin: 'http://localhost:5173', 
   credentials: true, 
 };
 
